2abd0b4b47cb285e78cfab5b96a41e968bc1199a
Chioma23 <sarahchibabe1@gmail.com>
2023-02-24 05:03:46 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,16 @@
 
 /**
 * _isupper - Check for uppercase character
-* @c: the number to be checked
-* Return:1 for upper letter or 0 for any else
+* @c: the parameter to be checked
+*Return:1 or 0
 */
 
 int _isupper(int c)
 
 {
-	if (x >= 65 && x <= 90)
-	{
-	return (1);
-	}
-	return (0);
+	if (x >= "A" && x <= "Z")
+		return (1);
+	else
+		return (0);
 }
 
